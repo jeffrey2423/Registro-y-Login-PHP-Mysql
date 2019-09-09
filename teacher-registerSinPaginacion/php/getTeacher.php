9c77de82de9f9f46ef86e94524e27034c2a25611
JeffreyRios24 <jeffrey.rios@correounivalle.edu.co>
2019-09-09 10:02:21 -0500

    frontend desing terminado

--- teacher-registerSinPaginacion/php/getTeacher.php
+++ teacher-registerSinPaginacion/php/getTeacher.php
@@ -20,14 +20,14 @@
         while ($result=mysqli_fetch_assoc($executeLimit)) { 
 
            $html .= '
-           <div class="card my-3" style="max-width: 338px; height:370 ; display: inline-block;">
+           <div class="card my-3" style="max-width: 338px; max-height:370 ; display: inline-block;">
                 <img src="https://www.jennstrends.com/wp-content/uploads/2013/10/bad-profile-pic-2-768x768.jpeg" class="card-img-top"  alt="">
                 <div class="card-body">
                     <h5 class="card-title">'.$result['name'].'</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                 </div>
                 <div class="card-footer">
-                    <small class="text-muted">Creado a las</small>
+                    <small class="text-muted">Creado a las '.date_format(date_create($result['created_at']), 'g:ia \o\n l jS F Y').'</small>
 
                 </div>
             </div>
