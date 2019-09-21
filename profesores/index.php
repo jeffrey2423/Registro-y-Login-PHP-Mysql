93b29f2f641e914fdc5b837d1b1e45b5dc55a9a0
Jeffrey Rios <jeffrey.rios@correounivalle.edu.co>
2019-09-21 16:43:08 -0500

    app-web terminada

--- profesores/index.php
+++ profesores/index.php
@@ -21,7 +21,7 @@ if (isset($_SESSION['login'])){
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <img src="/imagenes/escuddi.png" alt="Icetex" width="5%" height="5%"><a class="navbar-brand" href="http://www.eldiamante.com.co/">IEO El Diamante</a>	
+  <!--<img src="/imagenes/escuddi.png" alt="Icetex" width="5%" height="5%">--><a class="navbar-brand" href="http://www.eldiamante.com.co/">IEO El Diamante</a>	
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
